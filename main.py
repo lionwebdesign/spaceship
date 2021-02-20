b8263bf45ee420c19b627b599833e72a59f1370a
Ruben Villalvazo <vim.ruben@hotmail.com>
2021-02-20 01:05:16 -0300

    Codigo y renderizacion de nave

--- main.py
+++ main.py
@@ -7,11 +7,27 @@ screen_height = 576
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
+#Clases 
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self, path, x_pos, y_pos, speed):
+        super().__init__()
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(center = (x_pos, y_pos))
+
+# Variables generales
+spaceship = SpaceShip('spaceship/assets/spaceship.png', screen_width/2, 500, 10)
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     
+    screen.fill((40, 38, 42))
+
+    spaceship_group.draw(screen)
+
     pygame.display.update()
     clock.tick(120)
